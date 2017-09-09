47ed5de2d3f78562a88440ed5370e099ed1c0566
seo <bravoyourlife528@gamil.com>
2017-09-09 20:23:01 +0900

    Device model error correct

--- Equalizer/app/src/main/java/com/equalizer/hyosori/equalizer/model/DeviceInfo.java
+++ Equalizer/app/src/main/java/com/equalizer/hyosori/equalizer/model/DeviceInfo.java
@@ -3,19 +3,19 @@
  * Created by samsung on 2017-09-09.
  */
 
-public class deviceInfo {
+public class DeviceInfo {
     String model;
     int freq1_mean;
     int freq2_mean;
     int freq3_mean;
     int freq4_mean;
 
-    public  deviceInfo()
+    public  DeviceInfo()
     {
 
     }
 
-    public deviceInfo(String model, int freq1, int freq2, int freq3, int freq4)
+    public DeviceInfo(String model, int freq1, int freq2, int freq3, int freq4)
     {
         this.model = model;
         this.freq1_mean = freq1;
